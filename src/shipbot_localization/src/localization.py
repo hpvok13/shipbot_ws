c765fa90587778b78853bdb8f8fb232630be569e
Valmiki Kothare <valmik.kothare.vk@gmail.com>
2022-04-10 20:58:20 -0400

    New

--- src/shipbot_localization/src/localization.py
+++ src/shipbot_localization/src/localization.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+import ydlidar_ros_driver
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
