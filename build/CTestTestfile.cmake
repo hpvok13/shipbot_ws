71f5c8afcf4dd0f40b24f108322492a03e900315
Valmiki Kothare <valmik.kothare.vk@gmail.com>
2022-04-10 20:27:16 -0400

    First Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/theseus/shipbot_ws/src
+# Build directory: /home/theseus/shipbot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("shipbot")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/theseus/shipbot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/theseus/shipbot_ws/devel;/opt/ros/melodic'.split(';') if '/home/theseus/shipbot_ws/devel;/opt/ros/melodic' != '' else []
