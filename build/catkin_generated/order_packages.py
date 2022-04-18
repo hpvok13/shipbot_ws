7dbe00a63263325c7a5f4c30248ffc08eb8fc88d
Valmiki Kothare <valmiki.kothare.vk@gmail.com>
2022-04-18 17:53:56 -0400

    Added gmapping code, needs tuning

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/valmiki/shipbot_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/valmiki/ydlidar_ws/devel;/home/valmiki/catkin_ws/devel;/home/valmiki/shipbot_ws/devel;/opt/ros/noetic'.split(';') if '/home/valmiki/ydlidar_ws/devel;/home/valmiki/catkin_ws/devel;/home/valmiki/shipbot_ws/devel;/opt/ros/noetic' != '' else []
